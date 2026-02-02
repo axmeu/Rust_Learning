f70f9dc894e46967d7148d4d5dc9f97acd403aec
Axel Meunier <axmeu079@gmail.com>
2026-02-02 21:39:36 +0100

    Add conv_num_types

---
+++ conv_num_types/src/main.rs
@@ -0,0 +1,10 @@
+/*
+Credit:
+https://www.rustfinity.com/practice/rust/challenges/converting-numerical-types
+Implement a function called numerical_type_conversion that takes an i32 as input and returns it as a u32. You should use the as keyword to perform this conversion.
+*/
+
+pub fn numerical_type_conversion(n: i32) -> u32 {
+    // Implement here
+    n as u32
+}
